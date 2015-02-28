2742e5c52be09b57d6741d79c0ab2b5edc98f66f
Rafal Truszkowski <truszko1@illinois.edu>
2015-02-28 01:14:00 -0600

    signup/login implemented

--- Messaging/src/com/truszko1/messaging/LoginActivity.java
+++ Messaging/src/com/truszko1/messaging/LoginActivity.java
@@ -1,30 +1,90 @@
 package com.truszko1.messaging;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class LoginActivity extends Activity {
 
-	
 	protected TextView mSignupTextView;
-	
+
+	protected EditText mUsername;
+	protected EditText mPassword;
+	protected Button mLoginButton;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
-		
+
 		mSignupTextView = (TextView) findViewById(R.id.signupText);
 		mSignupTextView.setOnClickListener(new View.OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
-				Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+				Intent intent = new Intent(LoginActivity.this,
+						SignupActivity.class);
 				startActivity(intent);
+			}
+		});
+
+		mUsername = (EditText) findViewById(R.id.usernameField);
+		mPassword = (EditText) findViewById(R.id.passwordField);
+		mLoginButton = (Button) findViewById(R.id.loginButton);
+		mLoginButton.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				String username = mUsername.getText().toString().trim();
+				String password = mPassword.getText().toString().trim();
+
+				if (username.isEmpty() || password.isEmpty()) {
+					AlertDialog.Builder builder = new AlertDialog.Builder(
+							LoginActivity.this);
+					builder.setMessage(R.string.login_error_message)
+							.setTitle(R.string.login_error_title)
+							.setPositiveButton(android.R.string.ok, null);
+					AlertDialog dialog = builder.create();
+					dialog.show();
+				} else {
+					ParseUser.logInInBackground(username, password,
+							new LogInCallback() {
+								public void done(ParseUser user,
+										ParseException e) {
+									if (user != null) {
+										Intent intent = new Intent(
+												LoginActivity.this,
+												MainActivity.class);
+										intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+										intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+										startActivity(intent);
+									} else {
+										AlertDialog.Builder builder = new AlertDialog.Builder(
+												LoginActivity.this);
+										builder.setMessage(e.getMessage())
+												.setTitle(
+														R.string.signup_error_title)
+												.setPositiveButton(
+														android.R.string.ok,
+														null);
+										AlertDialog dialog = builder.create();
+										dialog.show();
+									}
+								}
+							});
+				}
+
 			}
 		});
 	}
--- Messaging/src/com/truszko1/messaging/SignupActivity.java
+++ Messaging/src/com/truszko1/messaging/SignupActivity.java
@@ -1,16 +1,83 @@
 package com.truszko1.messaging;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class SignupActivity extends Activity {
+
+	protected EditText mUsername;
+	protected EditText mPassword;
+	protected EditText mEmail;
+	protected Button mSignupButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_signup);
+
+		mUsername = (EditText) findViewById(R.id.usernameField);
+		mPassword = (EditText) findViewById(R.id.passwordField);
+		mEmail = (EditText) findViewById(R.id.emailField);
+		mSignupButton = (Button) findViewById(R.id.signupButton);
+		mSignupButton.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				String username = mUsername.getText().toString().trim();
+				String password = mPassword.getText().toString().trim();
+				String email = mEmail.getText().toString().trim();
+
+				if (username.isEmpty() || password.isEmpty() || email.isEmpty()) {
+					AlertDialog.Builder builder = new AlertDialog.Builder(
+							SignupActivity.this);
+					builder.setMessage(R.string.signup_error_message)
+							.setTitle(R.string.signup_error_title)
+							.setPositiveButton(android.R.string.ok, null);
+					AlertDialog dialog = builder.create();
+					dialog.show();
+				} else {
+					ParseUser newUser = new ParseUser();
+					newUser.setUsername(username);
+					newUser.setPassword(password);
+					newUser.setEmail(email);
+					newUser.signUpInBackground(new SignUpCallback() {
+
+						@Override
+						public void done(ParseException e) {
+							if (e == null) {
+								// success!
+								Intent intent = new Intent(SignupActivity.this,
+										MainActivity.class);
+								intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+								intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+								startActivity(intent);
+							} else {
+								AlertDialog.Builder builder = new AlertDialog.Builder(
+										SignupActivity.this);
+								builder.setMessage(e.getMessage())
+										.setTitle(R.string.signup_error_title)
+										.setPositiveButton(android.R.string.ok,
+												null);
+								AlertDialog dialog = builder.create();
+								dialog.show();
+							}
+						}
+					});
+				}
+
+			}
+		});
 	}
 
 	@Override
