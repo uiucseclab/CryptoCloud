b25603b428513feb1d26ead572aa247d13451aeb
Rafal Truszkowski <truszko1@illinois.edu>
2015-03-02 02:51:16 -0600

    can record videos which are stored in the Gallery

--- Messaging/src/com/truszko1/messaging/MainActivity.java
+++ Messaging/src/com/truszko1/messaging/MainActivity.java
@@ -19,13 +19,13 @@ import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.Window;
 import android.widget.Toast;
 
 import com.parse.ParseUser;
 
 public class MainActivity extends FragmentActivity implements
 		ActionBar.TabListener {
+
 	public static final String TAG = MainActivity.class.getSimpleName();
 
 	public static final int TAKE_PHOTO_REQUEST = 0;
@@ -38,19 +38,16 @@ public class MainActivity extends FragmentActivity implements
 
 	protected Uri mMediaUri;
 
-	private ViewPager mViewPager;
-	private TabsPagerAdapter mAdapter;
-
 	protected DialogInterface.OnClickListener mDialogListener = new DialogInterface.OnClickListener() {
 		@Override
 		public void onClick(DialogInterface dialog, int which) {
 			switch (which) {
-			case 0:
-				// take picture
+			case 0: // Take picture
 				Intent takePhotoIntent = new Intent(
 						MediaStore.ACTION_IMAGE_CAPTURE);
-				mMediaUri = getOutPutMediaUri(MEDIA_TYPE_IMAGE);
+				mMediaUri = getOutputMediaFileUri(MEDIA_TYPE_IMAGE);
 				if (mMediaUri == null) {
+					// display an error
 					Toast.makeText(MainActivity.this,
 							R.string.error_external_storage, Toast.LENGTH_LONG)
 							.show();
@@ -60,19 +57,32 @@ public class MainActivity extends FragmentActivity implements
 					startActivityForResult(takePhotoIntent, TAKE_PHOTO_REQUEST);
 				}
 				break;
-			case 1:
-				// take video
+			case 1: // Take video
+				Intent videoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+				mMediaUri = getOutputMediaFileUri(MEDIA_TYPE_VIDEO);
+				if (mMediaUri == null) {
+					// display an error
+					Toast.makeText(MainActivity.this,
+							R.string.error_external_storage, Toast.LENGTH_LONG)
+							.show();
+				} else {
+					videoIntent.putExtra(MediaStore.EXTRA_OUTPUT, mMediaUri);
+					videoIntent.putExtra(MediaStore.EXTRA_DURATION_LIMIT, 10);
+					videoIntent.putExtra(MediaStore.EXTRA_VIDEO_QUALITY, 0); // 0
+																				// =
+																				// lowest
+																				// res
+					startActivityForResult(videoIntent, TAKE_VIDEO_REQUEST);
+				}
 				break;
-			case 2:
-				// choose picture
+			case 2: // Choose picture
 				break;
-			case 3:
-				// choose video
+			case 3: // Choose video
 				break;
 			}
 		}
 
-		private Uri getOutPutMediaUri(int mediaType) {
+		private Uri getOutputMediaFileUri(int mediaType) {
 			// To be safe, you should check that the SDCard is mounted
 			// using Environment.getExternalStorageState() before doing this.
 			if (isExternalStorageAvailable()) {
@@ -129,19 +139,31 @@ public class MainActivity extends FragmentActivity implements
 		}
 	};
 
+	/**
+	 * The {@link android.support.v4.view.PagerAdapter} that will provide
+	 * fragments for each of the sections. We use a
+	 * {@link android.support.v4.app.FragmentPagerAdapter} derivative, which
+	 * will keep every loaded fragment in memory. If this becomes too memory
+	 * intensive, it may be best to switch to a
+	 * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+	 */
+	TabsPagerAdapter mTabsPagerAdapter;
+
+	/**
+	 * The {@link ViewPager} that will host the section contents.
+	 */
+	ViewPager mViewPager;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
-		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
-
 		setContentView(R.layout.activity_main);
 
 		ParseUser currentUser = ParseUser.getCurrentUser();
-		if (currentUser != null) {
-
-		} else {
+		if (currentUser == null) {
 			navigateToLogin();
+		} else {
+			Log.i(TAG, currentUser.getUsername());
 		}
 
 		// Set up the action bar.
@@ -150,11 +172,12 @@ public class MainActivity extends FragmentActivity implements
 
 		// Create the adapter that will return a fragment for each of the three
 		// primary sections of the app.
-		mAdapter = new TabsPagerAdapter(this, getSupportFragmentManager());
+		mTabsPagerAdapter = new TabsPagerAdapter(this,
+				getSupportFragmentManager());
 
 		// Set up the ViewPager with the sections adapter.
 		mViewPager = (ViewPager) findViewById(R.id.pager);
-		mViewPager.setAdapter(mAdapter);
+		mViewPager.setAdapter(mTabsPagerAdapter);
 
 		// When swiping between different sections, select the corresponding
 		// tab. We can also use ActionBar.Tab#select() to do this if we have
@@ -168,15 +191,15 @@ public class MainActivity extends FragmentActivity implements
 				});
 
 		// For each of the sections in the app, add a tab to the action bar.
-		for (int i = 0; i < mAdapter.getCount(); i++) {
+		for (int i = 0; i < mTabsPagerAdapter.getCount(); i++) {
 			// Create a tab with text corresponding to the page title defined by
 			// the adapter. Also specify this Activity object, which implements
 			// the TabListener interface, as the callback (listener) for when
 			// this tab is selected.
 			actionBar.addTab(actionBar.newTab()
-					.setText(mAdapter.getPageTitle(i)).setTabListener(this));
+					.setText(mTabsPagerAdapter.getPageTitle(i))
+					.setTabListener(this));
 		}
-
 	}
 
 	@Override
@@ -188,10 +211,7 @@ public class MainActivity extends FragmentActivity implements
 					Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
 			mediaScanIntent.setData(mMediaUri);
 			sendBroadcast(mediaScanIntent);
-		} else if (resultCode == RESULT_CANCELED) {
-			Toast.makeText(MainActivity.this, "no photos? :(",
-					Toast.LENGTH_LONG).show();
-		} else {
+		} else if (resultCode != RESULT_CANCELED) {
 			Toast.makeText(MainActivity.this, R.string.error, Toast.LENGTH_LONG)
 					.show();
 		}
@@ -199,8 +219,8 @@ public class MainActivity extends FragmentActivity implements
 
 	private void navigateToLogin() {
 		Intent intent = new Intent(this, LoginActivity.class);
-		intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
 		intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
 		startActivity(intent);
 	}
 
@@ -213,46 +233,40 @@ public class MainActivity extends FragmentActivity implements
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
+		int itemId = item.getItemId();
 
-		switch (id) {
+		switch (itemId) {
 		case R.id.action_logout:
 			ParseUser.logOut();
 			navigateToLogin();
-			break;
 		case R.id.action_edit_friends:
 			Intent intent = new Intent(this, EditFriendsActivity.class);
 			startActivity(intent);
-			break;
 		case R.id.action_camera:
 			AlertDialog.Builder builder = new AlertDialog.Builder(this);
 			builder.setItems(R.array.camera_choices, mDialogListener);
 			AlertDialog dialog = builder.create();
 			dialog.show();
 		}
+
 		return super.onOptionsItemSelected(item);
-	}
-
-	@Override
-	public void onTabReselected(ActionBar.Tab tab,
-			FragmentTransaction fragmentTransaction) {
-
 	}
 
 	@Override
 	public void onTabSelected(ActionBar.Tab tab,
 			FragmentTransaction fragmentTransaction) {
+		// When the given tab is selected, switch to the corresponding page in
+		// the ViewPager.
 		mViewPager.setCurrentItem(tab.getPosition());
-
 	}
 
 	@Override
 	public void onTabUnselected(ActionBar.Tab tab,
 			FragmentTransaction fragmentTransaction) {
-
 	}
 
+	@Override
+	public void onTabReselected(ActionBar.Tab tab,
+			FragmentTransaction fragmentTransaction) {
+	}
 }
