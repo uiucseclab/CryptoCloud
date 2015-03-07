29bf31148958679aaf52e5149209f9670135c7d3
Rafal Truszkowski <truszko1@illinois.edu>
2015-03-06 23:39:13 -0600

    cleanup

--- Messaging/src/com/truszko1/messaging/ParseConstants.java
+++
@@ -1,6 +0,0 @@
-package com.truszko1.messaging;
-
-public final class ParseConstants {
-	public static final String KEY_USERNAME = "username";
-	public static final String KEY_FRIENDS_RELATION = "friendsRelation";
-}
